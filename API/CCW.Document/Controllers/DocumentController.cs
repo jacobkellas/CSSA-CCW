c9abf4e4caa43d038021da3fffdb0abe6c02d8dc
Ofelia Porumb <Ofelia.Porumb@insight.com>
2022-10-17 17:18:35 -0700

    upload document to azure stoarge

--- API/CCW.Document/Controllers/DocumentController.cs
+++ API/CCW.Document/Controllers/DocumentController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using Azure.Storage.Blobs.Models;
 
 namespace CCW.Document.Controllers
 {
@@ -7,63 +11,111 @@ namespace CCW.Document.Controllers
     [ApiController]
     public class DocumentController : ControllerBase
     {
-        [HttpPost("upload", Name = "upload")]
+        [HttpPost("uploadImage", Name = "uploadImage")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UploadImage(
+         IFormFile fileToPersist,
+         string saveAsFileName,
+         CancellationToken cancellationToken)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=sdsdtestdoc;AccountKey=WeXIMJVMrWUYV2p4q5gBxIQtPvQ/D6jvk67uN7tlPpoTJx9eu9FROOhITsnKSdaa0IZdQlfpuK91+AStA0u+hA==;EndpointSuffix=core.usgovcloudapi.net";
+            string containerName = "ccw-documents";
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+
+            try
+            {
+                // Get a reference to a blob
+                BlobClient blob = container.GetBlobClient(saveAsFileName);
+
+                // Open the file and upload its data
+                using (Stream file = fileToPersist.OpenReadStream())
+                {
+                    blob.Upload(file, new BlobHttpHeaders { ContentType = "image/jpeg" });
+                }
+
+                var uri = blob.Uri.AbsoluteUri;
+            }
+            catch
+            {
+                // log error
+            }
+            
+            return Ok();
+        }
+
+        [HttpPost("uploadFile", Name = "uploadFile")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> UploadFile(
-         IFormFile file,
-         CancellationToken cancellationToken)
+            IFormFile fileToPersist,
+            string saveAsFileName,
+            CancellationToken cancellationToken)
         {
-            if (CheckIfExcelFile(file))
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=sdsdtestdoc;AccountKey=WeXIMJVMrWUYV2p4q5gBxIQtPvQ/D6jvk67uN7tlPpoTJx9eu9FROOhITsnKSdaa0IZdQlfpuK91+AStA0u+hA==;EndpointSuffix=core.usgovcloudapi.net";
+            string containerName = "ccw-documents";
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+
+            try
             {
-                await WriteFile(file);
+                // Get a reference to a blob
+                BlobClient blob = container.GetBlobClient(saveAsFileName);
+
+                // Open the file and upload its data
+                using (Stream file = fileToPersist.OpenReadStream())
+                {
+                    blob.Upload(file, new BlobHttpHeaders { ContentType = "application/pdf" });
+                }
+
+                var uri = blob.Uri.AbsoluteUri;
             }
-            else
+            catch
             {
-                return BadRequest(new { message = "Invalid file extension" });
+                // log error
             }
 
             return Ok();
         }
 
-        private bool CheckIfExcelFile(IFormFile file)
-        {
-            var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
-            return (extension == ".xlsx" || extension == ".xls"); // Change the extension based on your need
-        }
 
-        private async Task<bool> WriteFile(IFormFile file)
-        {
-            bool isSaveSuccess = false;
-            string fileName;
-            try
-            {
-                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
-                fileName = DateTime.Now.Ticks + extension; //Create a new Name for the file due to security reasons.
+        //private bool CheckIfExcelFile(IFormFile file)
+        //{
+        //    var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+        //    return (extension == ".xlsx" || extension == ".xls"); // Change the extension based on your need
+        //}
 
-                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
+        //private async Task<bool> WriteFile(IFormFile file)
+        //{
+        //    bool isSaveSuccess = false;
+        //    string fileName;
+        //    try
+        //    {
+        //        var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+        //        fileName = DateTime.Now.Ticks + extension; //Create a new Name for the file due to security reasons.
 
-                if (!Directory.Exists(pathBuilt))
-                {
-                    Directory.CreateDirectory(pathBuilt);
-                }
+        //        var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
 
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
-                   fileName);
+        //        if (!Directory.Exists(pathBuilt))
+        //        {
+        //            Directory.CreateDirectory(pathBuilt);
+        //        }
 
-                using (var stream = new FileStream(path, FileMode.Create))
-                {
-                    await file.CopyToAsync(stream);
-                }
+        //        var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
+        //           fileName);
 
-                isSaveSuccess = true;
-            }
-            catch (Exception e)
-            {
-                //log error
-            }
+        //        using (var stream = new FileStream(path, FileMode.Create))
+        //        {
+        //            await file.CopyToAsync(stream);
+        //        }
 
-            return isSaveSuccess;
-        }
+        //        isSaveSuccess = true;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        //log error
+        //    }
+
+        //    return isSaveSuccess;
+        //}
     }
 }
