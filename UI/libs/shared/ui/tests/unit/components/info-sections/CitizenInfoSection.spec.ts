7971b46f53334a6a541c63a782f6830edad67652
jeremiahbooth-insight <113918667+jeremiahbooth-insight@users.noreply.github.com>
2022-10-20 07:01:11 -0600

    AB#1924 Create the new form for the renewal application

---
+++ UI/libs/shared/ui/tests/unit/components/info-sections/CitizenInfoSection.spec.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import Vuetify from 'vuetify';
+import CitizenInfoSection from '@shared-ui/components/info-sections/CitizenInfoSection.vue';
+import { createLocalVue, mount } from '@vue/test-utils';
+
+const localVue = createLocalVue();
+
+describe('CitizenInfoSection', () => {
+  let vuetify;
+  let wrapper;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    //@ts-ignore
+    wrapper = mount(CitizenInfoSection, {
+      localVue,
+      vuetify,
+      mocks: {
+        $t: text => text,
+      },
+      propsData: {
+        citizenshipInfo: {
+          citizen: true,
+          militaryStatus: 'active',
+        },
+      },
+    });
+  });
+
+  it('Should match the snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
---
+++ UI/libs/shared/ui/tests/unit/components/info-sections/DOBinfoSection.spec.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import Vuetify from 'vuetify';
+import DOBinfoSection from '@shared-ui/components/info-sections/DOBinfoSection.vue';
+import { createLocalVue, mount } from '@vue/test-utils';
+
+const localVue = createLocalVue();
+
+describe('DOBinfoSection', () => {
+  let vuetify;
+  let wrapper;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    //@ts-ignore
+    wrapper = mount(DOBinfoSection,  {
+      localVue,
+      vuetify,
+      mocks: {
+        $t: text => text,
+      },
+      propsData: {
+        DOBInfo: {
+          birthDate: '01/01/01',
+          birthCity: 'city',
+          birthState: 'state',
+          birthCountry: 'country',
+          currentAge: 'age',
+        },
+      },
+    });
+  });
+
+  it('Should match the snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
