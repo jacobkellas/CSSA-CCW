b1a5ccf8efcbed72d67ab333739a0436b9d55a3e
Ofelia Porumb <Ofelia.Porumb@insight.com>
2022-12-13 11:32:42 -0700

    Fix formating

--- API/CCW.Application/Entities/Weapon.cs
+++ API/CCW.Application/Entities/Weapon.cs
@@ -1,10 +1,9 @@
-﻿namespace CCW.Application.Entities
+﻿namespace CCW.Application.Entities;
+
+public class Weapon
 {
-    public class Weapon
-    {
-        public string? Make { get; set; }
-        public string? Model { get; set; }
-        public string? Caliber { get; set; }
-        public string? SerialNumber { get; set; }
-    }
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public string? Caliber { get; set; }
+    public string? SerialNumber { get; set; }
 }
