263dd90c5b257bc6b9ce777645800f12a4f9d4de
Ofelia Porumb <Ofelia.Porumb@insight.com>
2023-01-05 13:39:09 -0700

    userid for update app

--- API/CCW.Document/Controllers/DocumentController.cs
+++ API/CCW.Document/Controllers/DocumentController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CCW.Document.Services;
 using Microsoft.AspNetCore.Authorization;
+using iText.Forms;
+using iText.Kernel.Pdf;
+using iText.Forms.Fields;
+using Org.BouncyCastle.Crypto.IO;
+using System.Reflection.Metadata;
 
 namespace CCW.Document.Controllers;
 
@@ -264,8 +269,8 @@ public class DocumentController : ControllerBase
     }
 
 
-    [Authorize(Policy = "B2CUsers")]
-    [Authorize(Policy = "AADUsers")]
+    //[Authorize(Policy = "B2CUsers")]
+    //[Authorize(Policy = "AADUsers")]
     [HttpGet("getFile", Name = "getFile")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
@@ -273,125 +278,122 @@ public class DocumentController : ControllerBase
         string fileName,
         CancellationToken cancellationToken)
     {
-        //iTextSharp.text.Document document = new iTextSharp.text.Document();
+        var agencyFileName = "Template_2022";
 
-        // MemoryStream stream = new MemoryStream();
+        IDictionary<string, PdfFormField> fields;
+        MemoryStream outStream = new MemoryStream();
 
-        //try
-        //{
-        //    GetUserId(out var userId);
+        var pdfTemplate = await _azureStorage.DownloadAgencyFileAsync(agencyFileName, cancellationToken: default);
+        if (await pdfTemplate.ExistsAsync())
+        {
+            await pdfTemplate.DownloadToStreamAsync(outStream);
+            //Stream blobStream = pdfTemplate.OpenReadAsync().Result;
+            var stream = await pdfTemplate.OpenReadAsync();
+            var docFile = File(stream, pdfTemplate.Properties.ContentType);
 
-        //    PdfWriter pdfWriter = PdfWriter.GetInstance(document, stream);
-        //    pdfWriter.CloseStream = false;
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(outStream));
+            iText.Layout.Document docText = new iText.Layout.Document(pdfDoc);
 
-        //    MemoryStream ms = new MemoryStream();
-        //    fileName = "Template_2022";
-        //    var file = await _azureStorage.DownloadAgencyFileAsync(fileName, cancellationToken: default);
-        //    if (await file.ExistsAsync())
-        //    {
-        //        await file.DownloadToStreamAsync(ms);
-        //        Stream blobStream = file.OpenReadAsync().Result;
-        //    }
+            PdfReader pdfReader = new PdfReader(outStream);
+            PdfWriter pdfWriter = new PdfWriter(outStream);
+            PdfDocument doc = new PdfDocument(pdfReader, pdfWriter);
 
-        //    document.Open();
-        //    document.Add(new Paragraph("Hello World"));
-        //}
-        //catch (DocumentException de)
-        //{
-        //    Console.Error.WriteLine(de.Message);
-        //}
-        //catch (IOException ioe)
-        //{
-        //    Console.Error.WriteLine(ioe.Message);
-        //}
+            PdfAcroForm form = PdfAcroForm.GetAcroForm(doc, true);
+            form.SetGenerateAppearance(true);
 
-        //document.Close();
+            // Get the list of possible form fields so we can implement setting them
+            //var fields = form.GetFormFields();
+            //foreach(var item in fields)
+            //{
+            //    Console.WriteLine(item.Key);
+            //}
 
-        //stream.Flush(); //Always catches me out
-        //stream.Position = 0; //Not sure if this is required
+            // Sample setting the Last Name field
+            var lastName = form.GetField("order.data.application.lastname");
+            lastName.SetValue("McWhirter", true);
 
-        //return File(stream, "application/pdf", "DownloadName.pdf");
-        //try
-        //{
-        ////itext7
-        //MemoryStream stream = new MemoryStream();
+            var firstName = form.GetField("order.data.application.firstname");
+            firstName.SetValue("Leslie", true);
 
-        //MemoryStream ms = new MemoryStream();
-        //var agencyFileName = "Template_2022";
-        //var file = await _azureStorage.DownloadAgencyFileAsync(agencyFileName, cancellationToken: default);
-        //if (await file.ExistsAsync())
-        //{
-        //    await file.DownloadToStreamAsync(ms);
-        //    Stream blobStream = file.OpenReadAsync().Result;
-
-        //    PdfWriter pdfWriter = PdfWriter.GetInstance(document, ms);
-
-        //    using (PdfReader pdfReader = new PdfReader(agencyFileName))
-        //    {
-        //        using (PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileStream(agencyFileName, FileMode.Create)))
-        //        {
-        //            AcroFields pdfFormFields = pdfStamper.AcroFields;
-
-        //            pdfFormFields.SetField("11", "Porumb");
-        //            pdfFormFields.SetField("12", "Ofelia");
+            doc.Close();
 
 
-        //foreach (var field in this.textFields)
-        //{
-        //    //Locate the field
-        //    IList<AcroFields.FieldPosition> fieldLocations = pdfFormFields.GetFieldPositions(field.FieldName);
-        //    if (field.FieldText != null)
-        //    {
-        //        foreach (var location in fieldLocations)
-        //        {
-        //            canvas = pdfStamper.GetOverContent(location.page);
-        //            position = location.position;
+            //stream.Flush(); //Always catches me out
+            //stream.Position = 0; //Not sure if this is required
 
-        //            ct = new ColumnText(canvas);
-        //            ct.SetSimpleColumn(position);
-        //            ct.SetLeading(field.FixedLeading, field.MultipliedLeading);
-        //            ct.SetIndent(field.Indent, false);
-        //            ct.AddText(new Phrase(field.FieldText));//Insert the value for the field
-        //            ct.Go();
-        //        }
-
-        //        //Remove the field name so that it cannot be edited.
-        //        pdfFormFields.RemoveField(field.FieldName);
-        //    }
-        //}
+            //return File(stream, "application/pdf", "DownloadName.pdf");
 
 
-        //                pdfStamper.Close();
-        //            }
-        //        }
 
 
-        //    //using (FileStream outFile = new FileStream(blobStream, FileMode.Create))
-        //    //    {
-        //    //        PdfReader pdfReader = new PdfReader(blobStream);
-        //    //        PdfStamper pdfStamper = new PdfStamper(pdfReader, outFile);
-        //    //        AcroFields fields = pdfStamper.AcroFields;
+            //    using (PdfReader pdfReader = new PdfReader(agencyFileName))
+            //    {
+            //        using (PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileStream(agencyFileName, FileMode.Create)))
+            //        {
+            //            AcroFields pdfFormFields = pdfStamper.AcroFields;
 
-        //    //        fields.SetField("11", "Porumb");
-        //    //        fields.SetField("12", "Ofelia");
-
-        //    //        pdfStamper.FormFlattening = true;
+            //            pdfFormFields.SetField("11", "Porumb");
+            //            pdfFormFields.SetField("12", "Ofelia");
 
 
-        //            //Response.Headers.Append("Content-Disposition", "inline");
-        //            //Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            //foreach (var field in this.textFields)
+            //{
+            //    //Locate the field
+            //    IList<AcroFields.FieldPosition> fieldLocations = pdfFormFields.GetFieldPositions(field.FieldName);
+            //    if (field.FieldText != null)
+            //    {
+            //        foreach (var location in fieldLocations)
+            //        {
+            //            canvas = pdfStamper.GetOverContent(location.page);
+            //            position = location.position;
 
-        //            return File(ms, "application/pdf", "DownloadName.pdf");
-        //      //  }
-        //    }
+            //            ct = new ColumnText(canvas);
+            //            ct.SetSimpleColumn(position);
+            //            ct.SetLeading(field.FixedLeading, field.MultipliedLeading);
+            //            ct.SetIndent(field.Indent, false);
+            //            ct.AddText(new Phrase(field.FieldText));//Insert the value for the field
+            //            ct.Go();
+            //        }
 
+            //        //Remove the field name so that it cannot be edited.
+            //        pdfFormFields.RemoveField(field.FieldName);
+            //    }
+            //}
+
+
+            //                pdfStamper.Close();
+            //            }
+            //        }
+
+
+            //    //using (FileStream outFile = new FileStream(blobStream, FileMode.Create))
+            //    //    {
+            //    //        PdfReader pdfReader = new PdfReader(blobStream);
+            //    //        PdfStamper pdfStamper = new PdfStamper(pdfReader, outFile);
+            //    //        AcroFields fields = pdfStamper.AcroFields;
+
+            //    //        fields.SetField("11", "Porumb");
+            //    //        fields.SetField("12", "Ofelia");
+
+            //    //        pdfStamper.FormFlattening = true;
+
+
+            //            //Response.Headers.Append("Content-Disposition", "inline");
+            //            //Response.Headers.Add("X-Content-Type-Options", "nosniff");
+
+            //            return File(ms, "application/pdf", "DownloadName.pdf");
+            //      //  }
+            //    }
+
+          
+            //}
+            //catch (Exception e)
+            //{
+            //    var originalException = e.GetBaseException();
+            //    _logger.LogError(originalException, originalException.Message);
+            //    throw new Exception("An error occur while trying to download agency file.");
+        }
         return null!;
-        //}
-        //catch (Exception e)
-        //{
-        //    var originalException = e.GetBaseException();
-        //    _logger.LogError(originalException, originalException.Message);
-        //    throw new Exception("An error occur while trying to download agency file.");
     }
 
     private void GetUserId(out string? userId)
