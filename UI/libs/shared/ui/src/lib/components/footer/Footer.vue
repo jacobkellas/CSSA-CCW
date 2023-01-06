e9c3f730c782e8521bd19a819ef2274095501e00
Sharath Gaddameedi <102755748+sgaddameedi-insight@users.noreply.github.com>
2023-01-06 15:21:08 -0600

    Fix build version (#257)

--- UI/libs/shared/ui/src/lib/components/footer/Footer.vue
+++ UI/libs/shared/ui/src/lib/components/footer/Footer.vue
@@ -9,14 +9,17 @@
       cols="12"
     >
       © {{ new Date().getFullYear() }} — CSSA
-      {{ !isDEV ? `—  ${getVersion}` : '' }}
+      {{ isDEV ? `—  ${getVersion}` : '' }}
     </v-col>
   </v-footer>
 </template>
 <script setup lang="ts">
 import VERSION from '@shared-utils/version';
 import { computed } from 'vue';
+import { useAppConfigStore } from '@shared-ui/stores/configStore';
+
+const appConfigStore = useAppConfigStore();
 
 const getVersion = computed(() => VERSION);
-const isDEV = process.env.NODE_ENV === 'development';
+const isDEV = appConfigStore.getAppConfig.environmentName === 'DEV';
 </script>
