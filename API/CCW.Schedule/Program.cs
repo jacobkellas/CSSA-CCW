0cc99a5f4d916a13e9663c381fff042719335ce7
lmcwhirt-insight <86177157+lmcwhirt-insight@users.noreply.github.com>
2023-02-06 11:41:22 -0700

    Debugging swagger issue in Schedule API

--- API/CCW.Schedule/Program.cs
+++ API/CCW.Schedule/Program.cs
@@ -113,38 +113,38 @@ builder.Services
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Appointments CCW",
-        Version = "v1"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-                      "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-                      "Example: \"Bearer 1safsfsdfdfd\"",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Title = "Appointments CCW",
+//        Version = "v1"
+//    });
+//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+//    {
+//        Name = "Authorization",
+//        Type = SecuritySchemeType.ApiKey,
+//        Scheme = "Bearer",
+//        BearerFormat = "JWT",
+//        In = ParameterLocation.Header,
+//        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+//                      "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+//                      "Example: \"Bearer 1safsfsdfdfd\"",
+//    });
+//    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+//        {
+//            new OpenApiSecurityScheme {
+//                Reference = new OpenApiReference {
+//                    Type = ReferenceType.SecurityScheme,
+//                    Id = "Bearer"
+//                }
+//            },
+//            new string[] {}
+//        }
+//    });
+//});
 
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
@@ -155,18 +155,18 @@ builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-app.UseSwagger(o =>
-{
-    o.RouteTemplate = Constants.AppName + "/swagger/{documentname}/swagger.json";
-});
+//app.UseSwagger(o =>
+//{
+//    o.RouteTemplate = Constants.AppName + "/swagger/{documentname}/swagger.json";
+//});
 
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("v1/swagger.json", $"CCW {Constants.AppName} v1");
-    options.RoutePrefix = $"{Constants.AppName}/swagger";
+//app.UseSwaggerUI(options =>
+//{
+//    options.SwaggerEndpoint("v1/swagger.json", $"CCW {Constants.AppName} v1");
+//    options.RoutePrefix = $"{Constants.AppName}/swagger";
 
-    options.EnableTryItOutByDefault();
-});
+//    options.EnableTryItOutByDefault();
+//});
 app.UseHealthChecks("/health");
 
 app.UseCors("corsapp");
