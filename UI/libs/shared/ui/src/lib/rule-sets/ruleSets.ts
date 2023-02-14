289f7bcc97ca05438821844613abd21bce98eefc
jeremiahbooth-insight <113918667+jeremiahbooth-insight@users.noreply.github.com>
2023-02-14 16:45:08 -0700

    Update ruleSets.ts (#321)

--- UI/libs/shared/ui/src/lib/rule-sets/ruleSets.ts
+++ UI/libs/shared/ui/src/lib/rule-sets/ruleSets.ts
@@ -24,10 +24,11 @@ export const notRequiredPhoneRuleSet = [
     i18n.t('Must only contain numbers'),
 ];
 export const requireNameRuleSet = [
+  v => Boolean(!v.includes(" ")) || i18n.t("Cannot contain spaces"),
   v => Boolean(v) || i18n.t('Field is required'),
   v =>
     Boolean(v.match(/[a-zA-Z\s]+$/)) ||
-    i18n.t('Names may only contain letters and spaces'),
+    i18n.t('Names may only contain letters'),
 ];
 
 export const notRequiredNameRuleSet = [
