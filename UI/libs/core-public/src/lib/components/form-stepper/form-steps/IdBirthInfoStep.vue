b57b18986d1865adb8408046f35c937e7374de5b
nwolfosh <nathaniel.wolford@sdsheriff.gov>
2023-05-22 13:44:05 -0700

    Update IdBirthInfoStep.vue
    changed "ID Number" column to "Driver's License Number"

--- UI/libs/core-public/src/lib/components/form-stepper/form-steps/IdBirthInfoStep.vue
+++ UI/libs/core-public/src/lib/components/form-stepper/form-steps/IdBirthInfoStep.vue
@@ -22,8 +22,8 @@
           >
             <v-text-field
               v-model="model.application.idInfo.idNumber"
-              :label="$t('ID number')"
-              :rules="[v => !!v || $t('ID  number is required')]"
+              :label="$t('Driver\'s License Number')"
+              :rules="[v => !!v || $t('Driver\'s License Number is required')]"
               :dense="isMobile"
               outlined
               maxlength="25"
