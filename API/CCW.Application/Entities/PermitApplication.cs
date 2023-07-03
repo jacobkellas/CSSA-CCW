3fea649092ba011b7f53e9d6e0a661a98cf7a52c
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2023-07-03 09:55:47 -0700

    Feature/modify card 2 (#436)
    * fix: layout of permit card 2, submitted to licensing date time
    
    * fix: various bug fixes and code improvement
    
    * feat: move history to front end
    
    * feat: get rid of all these mappers
    
    * chore: remove unused variable
    
    * test: update unit tests
    
    * feat: update mappers for Agency Profile Settings API
    
    * feat: update schedule mappers
    
    * feat: update user profile api mappers
    
    * chore: admin code cleanup
    
    * chore: common code cleanup
    
    * fix: update admin controller
    
    * fix: update json values to save correctly in database

--- API/CCW.Application/Entities/PermitApplication.cs
+++ API/CCW.Application/Entities/PermitApplication.cs
@@ -4,14 +4,11 @@ namespace CCW.Application.Entities;
 
 public class PermitApplication
 {
-    [JsonProperty("application")]
     public Application Application { get; set; }
     [JsonProperty("id")]
     public Guid Id { get; set; }
     [JsonProperty("userId")]
     public string UserId { get; set; }
-    [JsonProperty("paymentHistory")]
     public PaymentHistory[] PaymentHistory { get; set; }
-    [JsonProperty("history")]
     public History[] History { get; set; }
 }
